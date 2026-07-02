48be675affe0d453727b36d8e6cecb035ed95246
ludmilla <ludmilla.paulino@dcx.ufpb.br>
2026-07-02 14:00:36 -0300

    Iniciando o exercício 8 de POO

--- src/main/java/br/ufpb/dcx/joalheria/ProgramaPrincipal.java
+++ src/main/java/br/ufpb/dcx/joalheria/ProgramaPrincipal.java
@@ -1,4 +1,29 @@
 package br.ufpb.dcx.joalheria;
 
 public class ProgramaPrincipal {
+
+    public static void main(String[] args) {
+
+        SistemaJoalheria sistema = new SistemaJoalheria();
+
+        Joia j1 = new Joia("Anel", "Ouro", 1500);
+        Joia j2 = new Joia("Colar", "Prata", 800);
+        Joia j3 = new Joia("Pulseira", "Diamante", -500);
+
+        try {
+
+            sistema.cadastrarJoia(j1);
+            sistema.cadastrarJoia(j2);
+            sistema.cadastrarJoia(j3);
+
+        } catch (Exception e) {
+
+            System.out.println("Erro: " + e.getMessage());
+
+        }
+
+        for (Joia joia : sistema.getJoias()) {
+            System.out.println(joia);
+        }
+    }
 }
--- src/main/java/br/ufpb/dcx/joalheria/SistemaJoalheria.java
+++ src/main/java/br/ufpb/dcx/joalheria/SistemaJoalheria.java
@@ -1,4 +1,26 @@
 package br.ufpb.dcx.joalheria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SistemaJoalheria {
+
+    private List<Joia> joias;
+
+    public SistemaJoalheria() {
+        this.joias = new ArrayList<>();
+    }
+
+    public void cadastrarJoia(Joia joia) throws Exception {
+
+        if (joia.getPreco() < 0) {
+            throw new Exception("Preço inválido");
+        }
+
+        joias.add(joia);
+    }
+
+    public List<Joia> getJoias() {
+        return joias;
+    }
 }
